a9b9195bf621fbd3bb98859a59d6fe5917fe37a8
Hunter Bertoson <44106297+hkbertoson@users.noreply.github.com>
2025-06-04 18:50:53 -0500

    Commit

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,9 @@
-type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
+type Runtime = import('@astrojs/cloudflare').Runtime<Env>;
 
 declare namespace App {
 	interface Locals extends Runtime {}
+}
+
+interface Window {
+	Alpine: import('alpinejs').Alpine;
 }
